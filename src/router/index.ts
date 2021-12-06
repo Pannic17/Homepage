09fa185587057cca2c06e5c5d2d8d5a4b0687e04
Pan Nic <pannic1984@outlook.com>
2021-12-06 12:42:35 +0800

    Built 0.6

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory} from "vue-router";
+import {createRouter, createWebHashHistory, createWebHistory} from "vue-router";
 
 import Home from "../pages/Home.vue"
 import About from "../pages/About.vue"
@@ -92,7 +92,7 @@ const routes = [
 ]
 
 const router = createRouter({
-        history: createWebHashHistory('/Homepage/'),
+        history: createWebHistory('/Homepage/'),
         routes
 })
 
